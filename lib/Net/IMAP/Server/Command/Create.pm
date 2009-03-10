800a49ff7ff58f79dea99ca964d50a0c987abc24
Alex Vandiver <cpan@chmrr.net>
2009-03-10 17:50:53 +0000

    Import of ALEXMV/Net-IMAP-Server-1.12 from CPAN.
    gitpan-cpan-distribution: Net-IMAP-Server
    gitpan-cpan-version:      1.12
    gitpan-cpan-path:         ALEXMV/Net-IMAP-Server-1.12.tar.gz
    gitpan-cpan-author:       ALEXMV
    gitpan-cpan-maturity:     released

--- lib/Net/IMAP/Server/Command/Create.pm
+++ lib/Net/IMAP/Server/Command/Create.pm
@@ -5,9 +5,6 @@ use strict;
 
 use base qw/Net::IMAP::Server::Command/;
 
-use Encode;
-use Encode::IMAPUTF7;
-
 sub validate {
     my $self = shift;
 
@@ -19,6 +16,16 @@ sub validate {
 
     my $mailbox = $self->connection->model->lookup( @options );
     return $self->no_command("Mailbox already exists") if $mailbox;
+
+    # This both ensures that the mailbox path is valid UTF-7, and that
+    # there aren't bogusly encoded characters (like '/' -> '&AC8-')
+    my $roundtrip = eval {
+        Encode::encode( 'IMAP-UTF-7',
+            Encode::decode( 'IMAP-UTF-7', $options[0] ) );
+    };
+
+    return $self->bad_command("Invalid UTF-7 encoding")
+        unless $roundtrip eq $options[0];
 
     return 1;
 }
